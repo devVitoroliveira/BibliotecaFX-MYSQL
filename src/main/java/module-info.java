6fc2c3249e4713151e39d665337f28254db22eec
Vitor Oliveira <dejesusvitoroliveira@gmail.com>
2025-12-20 00:26:39 -0300

    ajustando modulos

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,7 +2,6 @@ module biblioteca.fx {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.sql;
-    requires transitive javafx.graphics;
 
     opens biblioteca.fx to javafx.fxml;
     opens biblioteca.fx.Controller to javafx.fxml;
